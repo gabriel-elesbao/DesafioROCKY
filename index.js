2bbef610c24b429e39011526653e1e8b3a2cebcb
gabriel-elesbao <gabriel_elesbao1@hotmail.com>
2021-08-31 19:13:05 -0300

    Leitura do arquivo JSON

---
+++ index.js
@@ -0,0 +1,34 @@
+
+    
+const data = require('./broken-database.json')
+
+
+
+
+function corrigeFrase(frase){
+     const formatada = frase.replace(/[æ]/g,'a')
+        .replace(/[¢]/g,'c')
+        .replace(/[ø]/g,'o')
+        .replace(/[ß]/g,'b')
+
+    return formatada
+}
+
+
+const nameCorrigido = data
+    .map(item=> item.name)
+    .map(item=> corrigeFrase(item))
+
+const produtosCorrigidos = data.map(item=>{
+    return item.name=nameCorrigido.shift()
+})
+
+console.log(data)
+
+// newArrayWords.map(item => {
+//     if(correctWords.includes(item)){
+//       return  console.log(item)
+//     } else{
+//         return item.toLocaleUpperCase
+//     }
+// })
